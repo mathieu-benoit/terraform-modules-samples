caf78324634d0dd1c904cbdb3002f9879e8dd349
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2025-06-19 06:38:04 -0400

    gcp-pubsub-subscription-iam - project = var.project_id

--- gcp-pubsub-subscription-iam/main.tf
+++ gcp-pubsub-subscription-iam/main.tf
@@ -1,6 +1,7 @@
 resource "google_pubsub_subscription_iam_member" "iam_member" {
   for_each = var.principals
 
+  project      = var.project_id
   subscription = var.subscription_name
   role         = "roles/pubsub.subscriber"
   member       = each.key
